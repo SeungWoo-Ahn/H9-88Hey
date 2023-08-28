1266d8b11b00f5d3fc5c5128769748287dc1554a
SeungWoo-Ahn <rio970319@gmail.com>
2023-08-28 10:53:35 +0900

    #274 refactor: 컴포넌트 UI 변경과 코드 정리

--- android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/LoadingItems.kt
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/LoadingItems.kt
@@ -1,21 +1,16 @@
 package com.softeer.mycarchiving.ui.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import coil.ImageLoader
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
 import coil.decode.ImageDecoderDecoder
-import coil.imageLoader
 import coil.request.ImageRequest
 import coil.request.repeatCount
 import com.softeer.mycarchiving.R
